e136096909e3abb745f84b988a4b4a22f83bb73b
amanok2167 <amanok2167@gmail.com>
2019-02-17 17:03:30 +0900

    Update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.rst") as f:
 def main():
     setup(
         name="metadisassembler",
-        version="0.0.3",
+        version="0.0.4",
         url="https://github.com/the-metabolic-disassembler/metadisassembler",
         author="Kohei Amano",
         author_email="amanok2167@gmail.com",
